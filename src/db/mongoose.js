2757dc772843318e76b7a0c858aaa04603c02f8e
farees <fareezzhussain@gmail.com>
2020-08-27 02:23:59 +0530

    added validation using the validator module

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 
@@ -9,16 +10,47 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
 
 const User = mongoose.model('User',{
     name:{
-        type:String
+        type:String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type:String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
     },
     age:{
-        type:Number
+        type:Number,
+        default: 18,
+        validate(value){
+            if(value<0){
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    password:{
+        type:String,
+        required: true,
+        trim:true,
+        minlength: 7,
+        validator(value){
+            if(value.toLowerCase().includes('password')){
+                throw new Error('password cannot contain the "password"')
+            }
+        }
     }
 })
 
 const me = new User({
-    name: 'Farees Hussain',
-    age:'farees husain'
+    name: ' Farees Hussain  ',
+    email: 'fareezzhussaiN@gmail.com',
+    password: 'fareess'
 })
 
 me.save().then(()=>{
@@ -29,16 +61,18 @@ me.save().then(()=>{
 
 const Task = mongoose.model('Task',{
     description:{
-        type:String
+        type:String,
+        trim: true,
+        required: true,
     },
     completed:{
-        type:Boolean
+        type:Boolean,
+        default: false,
     }
 })
 
 const task = new Task({
-    description: 'learned how to save document using mongoose library',
-    completed: true
+    description: '       learned how to save document using mongoose library',
 })
 
 task.save().then(()=>{
